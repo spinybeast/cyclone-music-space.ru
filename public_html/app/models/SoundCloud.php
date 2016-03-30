001ba67c0f2933098a99da28081949a5402ec282
spiny <spiny_beast@mail.ru>
2016-03-30 11:21:35 +0300

    fix soundcloud

--- public_html/app/models/SoundCloud.php
+++ public_html/app/models/SoundCloud.php
@@ -4,10 +4,12 @@ class SoundCloud
 {
     const CLIENT_ID = 'f7844a0d2655ff6424cda2891baa462d';
     const CLIENT_SECRET = '36078b4b48e44eccf64c868e983cf804';
+    const RUWEB_IP = '91.201.40.13';
 
     public function getTracks()
     {
         $request = new Jyggen\Curl\Request("http://api.soundcloud.com/users/108057656/tracks.json?client_id=" . self::CLIENT_ID);
+        $request->setOption(CURLOPT_INTERFACE, self::RUWEB_IP);
         $request->execute();
 
         if ($request->isSuccessful()) {
